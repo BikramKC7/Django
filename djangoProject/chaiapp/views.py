65f60c2dbf9b9d47b265ff58892a88800ae24a68
unknown <kcvikram44@gmail.com>
2024-08-25 23:27:33 +0545

    till models created

--- djangoProject/chaiapp/views.py
+++ djangoProject/chaiapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Models
 
 # Create your views here.
 
 def all_chai(request):
+    chais = Models.objects.all
     return render(request,'chaiapp/chaiapp.html')
