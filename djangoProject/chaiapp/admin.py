7e89c710624d3ded724f2e34045cc4bd000fb696
unknown <kcvikram44@gmail.com>
2024-08-27 23:25:48 +0545

    till relationship of models

--- djangoProject/chaiapp/admin.py
+++ djangoProject/chaiapp/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Models
+from .models import Models, ChaiReview,Stores,ChaiCertificates
 
 # Register your models here.
-admin.site.register(Models)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra=2
+    
+class ModelsAdmin(admin.ModelAdmin):
+    list_display=('name','type','date_added')
+    inlines = [ChaiReviewInline]
+    
+class StoresAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    filter_horizontal = ('chai_varieties',)
+    
+class ChaiCertificatesAdmin(admin.ModelAdmin):
+    list_display=('chai','certificate_number')
+
+admin.site.register(Models,ModelsAdmin)
+admin.site.register(Stores,StoresAdmin)
+admin.site.register(ChaiCertificates,ChaiCertificatesAdmin)
+# admin.site.register(ChaiReview,ChaiReviewInline)
+
+
+
